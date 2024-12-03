a614b79e1df869f30b5e6e9896016828fde90645
Beryl1808 <dkwafo17@gmail.com>
2024-12-03 19:45:37 +0000

    3commit

---
+++ Data types.py
@@ -0,0 +1,9 @@
+#Explore the data types of different value
+a = 5
+print("type of a:",type(a))
+b = 2.5
+print("type of b:",type(b))
+c = "codingal"
+print("type of c:",type(c))
+d = True
+print("type of d:",type(d))
